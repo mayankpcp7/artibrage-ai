56147ccaa02ce093e4be666ea47c8d504e566c09
mayankpcp7 <jariwalmayank123@gmail.com>
2024-08-13 15:05:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "#1E1F27",
+        "dark-sky-blue": "#103852",
+        "light-sky": "#00A4FF",
+      },
+      fontSize: {
+        lg: "22px",
+        "custom-2xl": "28px",
+        "3xl": "36px",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+        lato: ["Lato"],
+        roboto: ["Roboto"],
+        oxygen: ["Oxygen"],
+        oxanium: ["Oxanium"],
+      },
+      lineHeight: {
+        113: "113%",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".text-shadow-darkblue": {
+            textShadow: "0px 4px 25px 0px #00A4FF",
+          },
+          ".text-shadow-skyblue": {
+            textShadow: "0px 4px 25px 0px #00A4FF",
+          },
+          ".text-shadow-white": {
+            textShadow: "0px 1.33px 33.3px 0px #FFFFFF",
+          },
+          ".text_shadow_blue": {
+            textShadow:
+              "0px 0px 20px 0px #00BFFF, 0px 0px 20px 0px #00BFFF, 0px 0px 0px 0px #00BFFF, 0px 0px 0px 0px #00BFFF, 0px 1px 0px 0px #00BFFF, 0px 1px 0px 0px #00BFFF",
+          },
+        },
+        ["responsive", "hover"]
+      );
+    },
+  ],
+};
